cbee55285503b5f41c92c82b3c8e9832192c0ec7
Geoffry Gambling <geoffrygambling@Geoffrys-iMac.local>
2019-01-31 16:52:45 -0800

    More testing of attributes. Need to find a way to set strokewidth becuase it always overrides any settings on foreground color

--- Setgame/ViewController.swift
+++ Setgame/ViewController.swift
@@ -22,29 +22,56 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var cardText: UIButton!{
         didSet {
-            cardChange()
+            colorizeText()
         }
     }
     
+    var triangle = "\u{25B2}"
+    var square = "\u{25A0}"
+    var circle = "\u{25CF}"
+
     var text = "\u{25B2}"
 
-//     MARK Stopping point. Placeholder is a single card symbol but with the attribute settings working in func cardChange()
-    private func cardChange() {
-       
-        var colorText: [NSAttributedString.Key: Any] = [
-//            .strokeWidth : -5,
-
-            .foregroundColor : UIColor.black.withAlphaComponent(0.15),
+    
+    var shaded =  [100, 0.15]
+    
+    
+    
+    // More testing of attributes. Need to find a way to set strokewidth becuase it always overrides any settings on foreground color
+    private func colorizeText() {
+        var textColor: [NSAttributedString.Key: Any] = [
+            .strokeWidth : 5,
+            .foregroundColor : UIColor.black.withAlphaComponent(CGFloat(shaded[0])),
             .font : UIFont.systemFont(ofSize : 50)
             
-            ]
+        ]
         
-        let  cardAttributes = NSAttributedString(string: text, attributes: colorText)
-       
-        cardText.setAttributedTitle(cardAttributes, for: [])
-
-
+        let textAttributes = NSAttributedString(string: text, attributes: textColor)
+        cardText.setAttributedTitle(textAttributes, for: [])
+        
     }
+    
+
+//     MARK Stopping point. Placeholder is a single card symbol but with the attribute settings working in func cardChange()
+
+    
+    
+    //    private func cardChange() {
+//
+//        var colorText: [NSAttributedString.Key: Any] = [
+//            .strokeWidth : -5,
+//
+//            .foregroundColor : UIColor.black.withAlphaComponent(100),
+//            .font : UIFont.systemFont(ofSize : 50)
+//
+//            ]
+//
+//        let  cardAttributes = NSAttributedString(string: text, attributes: colorText)
+//
+//        cardText.setAttributedTitle(cardAttributes, for: [])
+//
+//
+//    }
 
 }
 
