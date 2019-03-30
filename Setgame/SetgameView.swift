ef52136f40c09f59f235f0de8428b13a1943bacc
Geoffry Gambling <geoffrymichael@gmail.com>
2019-03-30 01:26:34 -0700

    More practicing of drawing.

--- Setgame/SetgameView.swift
+++ Setgame/SetgameView.swift
@@ -9,6 +9,89 @@
 import UIKit
 
 class SetgameView: UIView {
+    
+    
+    
+    
+//    private lazy var upperLeftCornerLabel = createCornerLabel()
+//
+//    private func createCornerLabel() -> UILabel {
+//        let label = UILabel()
+//        label.numberOfLines = 0
+//        addSubview(label)
+//
+//        return label
+//    }
+//
+//
+//
+//    private func configureCornerLabel(_ label: UILabel) {
+//        label.text = "🐮\n🐮\n🐮\n🐮\n🐮\n🐮\n🐮\n🐮\n🐮\n🐮\n🐮\n🐮"
+//
+//
+//        var shapeRect = CGRect(origin: label.center, size: CGSize.init(width: 50, height: 50))
+//
+//
+//
+//
+//
+//
+//        label.frame.size = CGSize.zero
+//        label.sizeToFit()
+//
+//    }
+    
+    
+//    lazy var viewSquare = CGRect(origin: CGPoint(x: bounds.midX, y: bounds.midY), size: CGSize.init(width: 100.0, height: 100.0))
+//
+//
+//
+//
+//
+//    override func layoutSubviews() {
+//
+//
+//        let shapeView = UIView(frame: viewSquare)
+//        shapeView.backgroundColor = UIColor.blue
+//
+//
+//
+//
+//        addSubview(shapeView)
+//
+//
+//
+//
+//
+////        let newView = UIView(frame: path)
+////
+////
+////
+////        newView.backgroundColor = UIColor.blue
+////        addSubview(newView)
+////
+//
+//
+//
+//
+////        func draw(_rect: CGRect) {
+////            let path = UIBezierPath()
+////                    path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: bounds.height * 0.10, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+////                    UIColor.red.setFill()
+////                    UIColor.black.setStroke()
+////                    path.lineWidth = 5.0
+////                    path.stroke()
+////                    path.fill()
+////                    path.move(to: CGPoint(x: bounds.midX, y: bounds.midY / 2))
+////        }
+//
+//        configureCornerLabel(upperLeftCornerLabel)
+//
+//
+//        upperLeftCornerLabel.center = CGPoint(x: bounds.midX, y: bounds.midY)
+//        upperLeftCornerLabel.backgroundColor = UIColor.red
+//
+//    }
 
     
     override func draw(_ rect: CGRect) {
@@ -17,29 +100,85 @@ class SetgameView: UIView {
         UIColor.white.setFill()
         roundedRect.fill()
         
-        let path = UIBezierPath()
-        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: bounds.height * 0.10, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
-        UIColor.red.setFill()
-        UIColor.black.setStroke()
-        path.lineWidth = 5.0
-        path.stroke()
-        path.fill()
-        path.move(to: CGPoint(x: bounds.midX, y: bounds.midY / 2))
+        drawShapes()
         
-        path.addArc(withCenter: path.currentPoint, radius: bounds.height * 0.10, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
-        path.stroke()
-        path.fill()
-        path.move(to: CGPoint(x: bounds.midX, y: bounds.midY * 1.5))
+//        let path = UIBezierPath()
+//        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: bounds.height * 0.10, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+//        UIColor.red.setFill()
+//        UIColor.black.setStroke()
+//        path.lineWidth = 5.0
+//        path.stroke()
+//        path.fill()
+//        path.move(to: CGPoint(x: bounds.midX, y: bounds.midY / 2))
+//
+//        path.addLine(to: path.currentPoint)
+//        path.stroke()
+//        path.fill()
+//        path.move(to: CGPoint(x: bounds.midX, y: bounds.midY * 1.5))
+//        path.stroke()
+//        path.fill()
         
-        path.addArc(withCenter: path.currentPoint, radius: bounds.height * 0.10, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
-        path.stroke()
-        path.fill()
+
+//        path.addArc(withCenter: path.currentPoint, radius: bounds.height * 0.10, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+//        path.stroke()
+//        path.fill()
+//        path.move(to: CGPoint(x: bounds.midX, y: bounds.midY * 1.5))
+//
+//        path.addArc(withCenter: path.currentPoint, radius: bounds.height * 0.10, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+//        path.stroke()
+//        path.fill()
         
+//        var shapeRect = CGRect(origin: CGPoint(x: bounds.midX, y: bounds.midY), size: CGSize(width: 100, height: 100))
+//
+//
+//
+//
+//        var squareView = UILabel(frame: shapeRect)
+//
+//        squareView.transform = CGAffineTransform.identity.translatedBy(x: -50, y: -50)
+//
+//        squareView.backgroundColor = UIColor.yellow
+//
+//        addSubview(squareView)
+//
         
         
         
         
     }
     
+    func drawShapes() {
+        
+        func drawCircle(origin: CGPoint) -> UIBezierPath {
+            let path = UIBezierPath()
+            path.addArc(withCenter: origin, radius: bounds.height * 0.10, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+            UIColor.red.setFill()
+            UIColor.black.setStroke()
+            path.lineWidth = 5.0
+            path.stroke()
+            path.fill()
+            path.move(to: origin)
+            return path
+        }
+        
+        var drawCirc = drawCircle(origin: CGPoint(x: bounds.midX, y: bounds.midY / 1.2))
+        
+        drawCirc.stroke()
+        drawCirc.fill()
+        
+        drawCirc = drawCircle(origin: CGPoint(x: bounds.midX, y: bounds.midY * 1.3))
+        
+        drawCirc.stroke()
+        drawCirc.fill()
+        
+        
+//        drawCircle(origin: CGPoint(x: bounds.midX, y: bounds.midY / 2), path: UIBezierPath())
+//
+//        drawCircle(origin: CGPoint(x: bounds.midX, y: bounds.midY * 1.5), path: UIBezierPath())
+        
+    }
+    
 
 }
+
+
