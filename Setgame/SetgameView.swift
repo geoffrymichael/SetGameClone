bac681ea6e425a06c9267d93ce270f4fba583ce4
Geoffry Gambling <geoffrymichael@gmail.com>
2019-03-29 18:49:28 -0700

    Created Cocoa Touch UIView SetgameView

---
+++ Setgame/SetgameView.swift
@@ -0,0 +1,19 @@
+//
+//  SetgameView.swift
+//  Setgame
+//
+//  Created by Geoffry Gambling on 3/29/19.
+//  Copyright © 2019 Geoffry Gambling. All rights reserved.
+//
+
+import UIKit
+
+class SetgameView: UIView {
+
+    
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    
+
+}
