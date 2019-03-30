b9a2d0d8e5e2001f1734ebd41d8d748461a2e8e0
Geoffry Gambling <geoffrymichael@gmail.com>
2019-03-29 19:37:31 -0700

    Tied shape size to card size so they resize when the cards do.

--- Setgame/SetgameView.swift
+++ Setgame/SetgameView.swift
@@ -12,7 +12,33 @@ class SetgameView: UIView {
 
     
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: 16)
+        roundedRect.addClip()
+        UIColor.white.setFill()
+        roundedRect.fill()
+        
+        let path = UIBezierPath()
+        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: bounds.height * 0.10, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+        UIColor.red.setFill()
+        UIColor.black.setStroke()
+        path.lineWidth = 5.0
+        path.stroke()
+        path.fill()
+        path.move(to: CGPoint(x: bounds.midX, y: bounds.midY / 2))
+        
+        path.addArc(withCenter: path.currentPoint, radius: bounds.height * 0.10, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+        path.stroke()
+        path.fill()
+        path.move(to: CGPoint(x: bounds.midX, y: bounds.midY * 1.5))
+        
+        path.addArc(withCenter: path.currentPoint, radius: bounds.height * 0.10, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+        path.stroke()
+        path.fill()
+        
+        
+        
+        
+        
     }
     
 
