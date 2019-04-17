be395f448e5e44a9ea5b64b6c51a565b4b576c1d
Geoffry Gambling <geoffrymichael@gmail.com>
2019-04-17 00:04:49 -0700

    Refining card matching and preparing to add draw three card function

--- Setgame/playingCardView.swift
+++ Setgame/playingCardView.swift
@@ -131,21 +131,7 @@ class playingCardView: UIView {
             configureCornerLabel(viewArray[card], gridNum: card)
         }
         
-        //        print(subviews[0].tag)
-        
-        
-        //TODO This is accessing the autolayout card. Maybe it should be replaced by attributes of first card in deck data set.
-        //
-        
-        //        let firstCard = view.subviews[0]
-        //
-        //        print("Thisisisiisisis first card",firstCard)
-        ////
-        ////
-        ////
-        ////
-        //        configureCornerLabel(firstCard, size: CGSize(width: self.view.bounds.width, height: self.view.bounds.height), center: CGPoint(x: self.view.bounds.midX, y: self.view.bounds.midY))
-        //        firstCard.bringSubviewToFront(viewArray[0])
+       
     }
     
     
@@ -164,65 +150,7 @@ class playingCardView: UIView {
     
     
     
-    //    @objc func handleTap() {
-    //
-    //        print("I pressed a view")
-    //
-    //
-    //
-    //    }
-    //
-    //
-    //    @IBOutlet var cardButton: [UIButton]! {
-    //        didSet {
-    //
-    //            newGame()
-    //            populateCards(shownCards: shownCards)
-    //            print(shownCards)
-    //
-    //        }
-    //    }
-    //
-    //    //This reveals the hidden cards three at a time.
-    //    @IBAction func dealThreeButton(_ sender: UIButton) {
-    //        if hiddenCardCount <= 23 {
-    //            if deck.cards.count > 0 {
-    //                for i in hiddenCardCount...hiddenCardCount + 2 {
-    //
-    //                    cardButton[i].backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-    //                    cardButton[i].isHidden = false
-    //                    cardButton[i].isEnabled = true
-    //                }
-    //            }
-    //
-    //        } else {
-    //            print("No more space")
-    //        }
-    //        hiddenCardCount += 3
-    //
-    //
-    //    }
-    //
-    //    //This is comparing the unique identifier "isChosen" int to compare chosen cards to cards from the deck and if the same, replace them.
-    //    func populateShownCards(chosenCards: [Card]) {
-    //        for i in 0..<chosenCards.count {
-    //            if chosenCards[i].isChosen == shownCards[i].isChosen {
-    //                shownCards[i] = deck.cards.remove(at: deck.cards.count.arc4random)
-    //                populateCards(shownCards: [shownCards[i]])
-    //                populateShownCards(chosenCards: [shownCards[i]])
-    //            }
-    //        }
-    //
-    //    }
-    //
-    //    //TODO This button does not yet work
-    //    @IBAction func newGameButton(_ sender: UIButton) {
-    //        shownCards = []
-    //        deck.clearChosen()
-    //        newGame()
-    //    }
-    //
-    //
+    
     //    //Newgame function which produces 12 shown card buttons with symbols and 12 hidden, non enabled cards. Note that the cards are populated(and 24 total cards removed from deck), but simply inactive and hidden.
     func newGame() {
         for _ in 0...11 {
@@ -231,79 +159,7 @@ class playingCardView: UIView {
         
         
     }
-    //
-    //
-    //    // Iterates through the first 24 cards and reads thier attributes and then shows them via the changeAtt function.
-    //    func populateCards(shownCards: [Card]) {
-    ////       var x = 0
-    //
-    //
-    //        for i in 0..<shownCards.count {
-    //            var btStroke : Int!
-    //            var btFloat : Float!
-    //            //This data is inherent in the data as text emoji
-    //            var btShape = shownCards[i].shape.rawValue
-    //
-    //
-    //            var btColor = UIColor.blue
-    //
-    //
-    //
-    //            if shownCards[i].color.rawValue == "purple" {
-    //                btColor = UIColor.purple
-    //            } else if shownCards[i].color.rawValue == "red" {
-    //                btColor = UIColor.red
-    //            } else if shownCards[i].fill.rawValue == "blue" {
-    //                btColor = UIColor.blue
-    //            }
-    //
-    //            if shownCards[i].fill.rawValue == "clear" {
-    //                btStroke = 5
-    //                btFloat = 5.00
-    //            } else if shownCards[i].fill.rawValue == "shaded" {
-    //                btStroke = -5
-    //                btFloat = 0.15
-    //            } else if shownCards[i].fill.rawValue == "filled" {
-    //                btStroke = -5
-    //                btFloat = 5.00
-    //            }
-    //
-    //
-    //            if shownCards[i].amount.rawValue == "two" {
-    //                btShape = btShape + btShape
-    //            } else if shownCards[i].amount.rawValue == "three" {
-    //                btShape = btShape + btShape + btShape
-    //            }
-    //
-    //            changeAtt(btShape, place: i, btStroke: btStroke, btFloat: btFloat, btColor: btColor)
-    //
-    ////            x += 1
-    //        }
-    ////        print(deck)
-    //        print(shownCards.count)
-    //        print(deck.cards.count)
-    //    }
-    //
-    //    //This is the visual function of the game. When used in populateCards it will translate the attirbute data into its visual representation.
-    //    func changeAtt(_ text: String, place: Int, btStroke: Int, btFloat: Float, btColor: UIColor) {
-    //        var textColor: [NSAttributedString.Key: Any] = [
-    //                    .strokeWidth : btStroke,
-    //                    .strokeColor : btColor,
-    //                    .foregroundColor : btColor.withAlphaComponent(CGFloat(btFloat)),
-    //                    .font : UIFont.systemFont(ofSize : 25)
-    //                ]
-    //
-    //                let textAttributes = NSAttributedString(string: text, attributes: textColor)
-    //                cardButton[place].setAttributedTitle(textAttributes, for: [])
-    //
-    //            }
-    //
-    //
-    //
-    //
-    //
-    //
-    //
+    
     
     //TODO Need to refine the card selecting logic (Having borders removed after three cards are selected)
     
@@ -343,7 +199,9 @@ class playingCardView: UIView {
                     
                     
                 }
-                
+                for i in 0..<viewArray.count {
+                    viewArray[i].removeFromSuperview()
+                }
                 viewArray = []
                 
                 for i in 0..<shownCards.count {
